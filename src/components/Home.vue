f2318f337b929e82243b9ba2b09407b94260299a
myanhon <pluh@protonmail.com>
2020-04-05 17:42:21 +0200

    logout/myorders in navigation bar

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,7 +4,6 @@
       <v-row no-gutters>
         <v-col class="box1" sm="6" md="5" lg="10">
           <component :is="selectedComponent"></component>
-          <v-btn @click="this.logout"> logout</v-btn>
         </v-col>
         <v-col
           class="box2  flex-lg-grow-0 "
@@ -109,13 +108,33 @@
         <template v-slot:activator="{ on }">
           <v-btn v-on="on">
             <span>Account</span>
-            <v-icon>fas fa-wine-bottle</v-icon>
+            <v-icon>fas fa-user</v-icon>
           </v-btn>
         </template>
-        <v-list>
-          <v-list-item v-for="(item, index) in items" :key="index">
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item>
+        <v-list class="text-center">
+          <v-list-item-content v-if="getStatus === 'Success'">
+            <v-list-item v-for="(item, index) in loggedIn" :key="index">
+              <v-list-item-title>
+                <router-link
+                  class="pointer"
+                  :to="item.url"
+                  @click="item.method()"
+                >
+                  {{ item.title }}</router-link
+                >
+                <p class="pointer" @click="logout()">
+                  Logout
+                </p>
+              </v-list-item-title>
+            </v-list-item>
+          </v-list-item-content>
+          <v-list-item-content v-else>
+            <v-list-item v-for="(item, index) in notLoggedIn" :key="index">
+              <v-list-item-title>
+                <router-link :to="item.url"> {{ item.title }}</router-link>
+              </v-list-item-title>
+            </v-list-item>
+          </v-list-item-content>
         </v-list>
       </v-menu>
     </v-bottom-navigation>
@@ -126,16 +145,22 @@ import Drinks from "./products/Drinks";
 import Lunch from "./products/Lunch";
 import Dinner from "./products/Dinner";
 import Wine from "./products/Wine";
+import Profile from "./user/Profile";
 import { mapActions, mapGetters } from "vuex";
 
 export default {
   data: () => ({
     selectedComponent: "Drinks",
-    items: [{ title: "My Orders", url: "/profile" }, { title: "Click Me" }]
+    loggedIn: [{ title: "My Orders", url: "/profile" }],
+    notLoggedIn: [
+      { title: "Login", url: "/login" },
+      { title: "Register", url: "/register" }
+    ]
   }),
-  components: { Drinks, Lunch, Dinner, Wine },
+  components: { Drinks, Lunch, Dinner, Wine, Profile },
   computed: {
-    ...mapGetters("Cart", ["getAllItems", "getTotalPrice"])
+    ...mapGetters("Cart", ["getAllItems", "getTotalPrice"]),
+    ...mapGetters("User", ["getStatus"])
   },
   methods: {
     ...mapActions("Cart", [
@@ -153,6 +178,16 @@ export default {
 </script>
 
 <style scoped>
+.v-application a {
+  text-decoration: none;
+  font-family: "Oswald", sans-serif;
+  color: black;
+}
+.pointer {
+  font-family: "Oswald", sans-serif;
+  cursor: pointer;
+  margin-top: 20%;
+}
 p {
   font-family: "Oswald", sans-serif;
 }
